8113f186c015cef66cf0b7a5644eca8c733bbd94
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-01-25 19:19:52 +0530

    used pool

--- api/todo.js
+++ api/todo.js
@@ -1,57 +1,86 @@
 const express = require('express');
 const route = express.Router();
 const db = require('../MyDB');
+const pool = require('../db_config');
 const bodyParser = require('body-parser');
 route.use(bodyParser.json());
 route.use(bodyParser.urlencoded({ extended:true}));
 
 //adds a task for the user
-route.put('/add',(req,res)=>{
+route.put('/add',async (req,res)=>{
     var body = req.body;
     var sql =`INSERT INTO todoApp (id,time,title,description) VALUES ('${body.id}','${body.time}','${body.title}', '${body.description}')`;
-    db.query(sql,function(err, result){
-        if(err) throw err;
+    // db.query(sql,function(err, result){
+    //     if(err) throw err;
+    //     res.json(result);
+    // });
+    try{
+        var result = await pool.query(sql);
         res.json(result);
-    });
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
 });
  // get all task for a perticular user
- route.get('/tasks',(req, res)=>{
+ route.get('/tasks',async (req, res)=>{
      var queryList = req.query;
      var username = queryList.username;
      var sql = `SELECT * FROM todoApp WHERE id='${username}'`;
-     db.query(sql,(err,result)=>{
-         if(err){
-             console.log(err);
-             res.sendStatus(500);
-         }else
-         res.json(result);
-     });
+    //  db.query(sql,(err,result)=>{
+    //      if(err){
+    //          console.log(err);
+    //          res.sendStatus(500);
+    //      }else
+    //      res.json(result);
+    //  });
+    try{
+        var result = await pool.query(sql);
+        res.json(result);
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
  });
 
  //delete a task for a username
- route.delete('/delete',(req, res)=>{
+ route.delete('/delete',async (req, res)=>{
      var body = req.body;
      var id = body.id;
      var time = body.time;
      var sql = `DELETE FROM todoApp WHERE id='${id}' AND time='${time}'`;
-     db.query(sql, (err, result)=>{
-         if(err) throw err;
-         res.json(result);
-     });
+    //  db.query(sql, (err, result)=>{
+    //      if(err) throw err;
+    //      res.json(result);
+    //  });
+    try{
+        var result = await pool.query(sql);
+        res.json(result);
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
  });
 
  //update task of a user with
- route.put('/update', (req, res)=>{
+ route.put('/update', async (req, res)=>{
      var body = req.body;
      var id = body.id;
      var time = body.time;
      var title = body.title;
      var description = body.description;
      var sql = `UPDATE todoApp SET title='${title}', description='${description}' WHERE id='${id}' AND time='${time}'`;
-     db.query(sql, (err, result)=>{
-         if(err) throw err;
-         res.json(result);
-     });
+    //  db.query(sql, (err, result)=>{
+    //      if(err) throw err;
+    //      res.json(result);
+    //  });
+    try{
+        var result = await pool.query(sql);
+        res.json(result);
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
  });
 
 module.exports = route;
--- api/userControl.js
+++ api/userControl.js
@@ -8,47 +8,76 @@ const pool = require('../db_config');
 route.use(bodyParser.json());
 route.use(bodyParser.urlencoded({ extended:true}));
 
-route.post('/signup',(req,res)=>{
+route.post('/signup',async (req,res)=>{
     var body = req.body;
     var pre_sql =`SELECT * FROM users WHERE username='${body.username}'`;
-    db.query(pre_sql,(err,result)=>{
-        if(err) throw err;
-        if(result.length == 0){
-            var sql =`INSERT INTO users (username, password) VALUES ('${body.username}', '${body.password}')`;
-            db.query(sql,function(err){
-                if(err) throw err;
-                console.log('inserted Created');
-                res.send('done');
-            });
-        }else
-        res.send('user already exists');
+    var sql =`INSERT INTO users (username, password) VALUES ('${body.username}', '${body.password}')`;
+    // db.query(pre_sql,(err,result)=>{
+    //     if(err) throw err;
+    //     if(result.length == 0){
+    //         db.query(sql,function(err){
+    //             if(err) throw err;
+    //             console.log('inserted Created');
+    //             res.send('done');
+    //         });
+    //     }else
+    //     res.send('user already exists');
         
-    });//end of pre_sql
-    
+    // });//end of pre_sql
+    try{
+        var pre_result = await pool.query(pre_sql);
+        if(pre_result.length == 0){
+            await pool.query(sql);
+            res.sendStatus(200);
+        }else{
+            res.send('user already exists');
+        }
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
 });
 
-route.post('/signin',(req,res)=>{
+route.post('/signin',async (req,res)=>{
     var body = req.body;
     var sql =`SELECT * FROM users WHERE username='${body.username}'`;
-    db.query(sql,function(err, result, fields){
-        if(err) throw err;
-        console.log(result);
+    // db.query(sql,function(err, result, fields){
+    //     if(err) throw err;
+    //     console.log(result);
+    //     if(result.length == 0)
+    //     res.sendStatus(404);
+    //     else if(result[0].password == body.password)
+    //     res.sendStatus(200);
+    //     else res.sendStatus(401);
+    // });
+    try{
+        var result = await pool.query(sql);
         if(result.length == 0)
         res.sendStatus(404);
         else if(result[0].password == body.password)
         res.sendStatus(200);
         else res.sendStatus(401);
-    });
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
 });
 
-route.post('/delete',(req,res)=>{
+route.post('/delete', async (req,res)=>{
     var body = req.body;
     var username = body.username;
     var sql = `DELETE FROM users WHERE username='${username}'`;
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
+    // db.query(sql,(err,result)=>{
+    //     if(err) throw err;
+    //     res.json(result);
+    // });
+    try{
+        var result = await pool.query(sql);
         res.json(result);
-    });
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
 });
 
 route.post('/alluser',async (req,res)=>{
